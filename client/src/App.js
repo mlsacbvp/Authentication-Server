ee0fdfa70531e4b32a4f776e6c2e26b0674b2396
bhoomikhurana <96761198+bhoomikhurana@users.noreply.github.com>
2023-10-31 19:47:53 +0530

    logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import { useRef, useState, useEffect } from "react";
 import "./App.css";
 import { uploadFile } from "./services/api";
-<<<<<<< HEAD
-
-=======
 // import nav from "./components/nav";
-import NavBar from "./components/NavBar";
->>>>>>> b2d392716cc90e88117cf3628e849b570fe5a1e3
+import NavBar from "./components/NavBar.js";
 function App() {
   const fileInputRef = useRef();
   const onUploadClick = () => {
     fileInputRef.current.click();
   };
   const [file, setFile] = useState("");
+  const [result, setResult] = useState("");
   console.log(file);
   useEffect(() => {
     const getImage = async () => {
@@ -20,47 +17,34 @@ function App() {
         const data = new FormData();
         data.append("name", file.name);
         data.append("file", file);
-        let response = await uploadFile();
+        let response = await uploadFile(data);
+        setResult(response.path);
       }
     };
     getImage();
   }, [file]);
   return (
-<<<<<<< HEAD
-    <>
-      <div className="container">
-        {/* <img src={logo} alt="banner" /> */}
-        <div className="wrapper">
-          <h1>FILE VORTEX</h1>
-          <p className="tag">Unleash the Power of Seamless Sharing</p>
-          <button onClick={() => onUploadClick()}>Upload</button>
-=======
-
-
-    
     <div className="container">
-    <header><NavBar/></header>
+      <header>
+        <NavBar />
+      </header>
       {/* <img src={logo} alt="banner" /> */}
       <div className="wrapper">
         <h1>FILE VORTEX</h1>
         <p className="tag">Unleash the Power of Seamless Sharing</p>
         <button onClick={() => onUploadClick()}>Upload</button>
->>>>>>> b2d392716cc90e88117cf3628e849b570fe5a1e3
 
-          <input
-            type="file"
-            ref={fileInputRef}
-            style={{ display: "none" }}
-            onChange={(e) => setFile(e.target.files[0])}
-          />
-        </div>
+        <input
+          type="file"
+          ref={fileInputRef}
+          style={{ display: "none" }}
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <a href={result} target="_blank">
+          {result}
+        </a>
       </div>
-<<<<<<< HEAD
-    </>
-=======
-
     </div>
->>>>>>> b2d392716cc90e88117cf3628e849b570fe5a1e3
   );
 }
 
